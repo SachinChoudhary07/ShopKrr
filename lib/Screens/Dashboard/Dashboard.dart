e8e4ba88fc18d4b2d1be2fef12a06ec8d5420ed0
Sachin Choudhary <sachin.dev2910@gmail.com>
2024-05-29 22:20:52 +0530

    Added dummy appBar

--- lib/Screens/Dashboard/Dashboard.dart
+++ lib/Screens/Dashboard/Dashboard.dart
@@ -10,8 +10,12 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Home"),
+      ),
+      body: const Column(
         children: [
           Center(
             child: Text("data"),
